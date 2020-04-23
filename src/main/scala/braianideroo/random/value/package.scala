8d9e043e97bb1b3d88e7ccbe5c83e449d001bf6a
Braian De Roo <braianideroo@gmail.com>
2020-04-23 17:01:01 -0300

    Added smoothing

--- src/main/scala/braianideroo/random/value/package.scala
+++ src/main/scala/braianideroo/random/value/package.scala
@@ -17,11 +17,13 @@
 package braianideroo.random
 
 import braianideroo.random
-import zio.ZIO
+import zio.{IO, ZIO}
 
 package object value {
   type RandomValue[R, E, A] = ZIO[SeedRandom with R, E, A]
   type RandomVIO[E, A] = RandomValue[Any, E, A]
+  type Probabilities[A] = Map[A, Double]
+  type SmoothF[A] = ZIO[Probabilities[A], Nothing, Probabilities[A]]
 
   case class Probability[A](value: A, probability: Double)
 
@@ -54,5 +56,32 @@ package object value {
 
     def fromIterable[A](iterable: Iterable[A]): RandomVIO[Nothing, Option[A]] =
       fromProbabilityIterable(iterable.map(x => Probability(x, 10)))
+
+    def fromMap[A](map: Probabilities[A]): RandomVIO[Nothing, Option[A]] = {
+      fromProbabilityIterable(map.map(x => Probability(x._1, x._2)))
+    }
+  }
+
+  object Smoothing {
+    def noSmoothing[A]: SmoothF[A] = ZIO.access[Probabilities[A]](x => x)
+
+    def priorSmoothing[A](prior: Double): SmoothF[A] =
+      for {
+        probabilities <- ZIO.access[Probabilities[A]](x => x)
+        res = probabilities.map(x => (x._1, x._2 + prior))
+      } yield res
+
+    def goodTuringSmoothing[A]: SmoothF[A] =
+      for {
+        probabilities <- ZIO.access[Probabilities[A]](x => x)
+        n1 = probabilities.count(_._2 == 1)
+        n0 = n1 / probabilities.values.sum
+        res = probabilities.map(x => (x._1, if (x._2 == 0) n0 else x._2))
+      } yield res
+  }
+
+  implicit class ProbabilitiesHelper[A](value: Probabilities[A]) {
+    def smooth(smoothF: SmoothF[A]): IO[Nothing, Probabilities[A]] =
+      smoothF.provide(value)
   }
 }
